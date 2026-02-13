6ba9c653f0892da4fcc39b871e69172298bfdad1
Goran Mržljak <goran.mrzljak@gmail.com>
2026-02-13 21:24:07 +0100

    add route for exchanges

--- app/routes/root.rb
+++ app/routes/root.rb
@@ -10,6 +10,18 @@ module Routes
       app.get "/env" do
         "EODHD_DIR: #{Config.eodhd_dir}"
       end
+
+      app.get "/exchanges" do
+        content_type :json
+        file_path = File.join(Config.eodhd_dir, "exchanges.json")
+        
+        unless File.exist?(file_path)
+          status 404
+          return { error: "File not found: #{file_path}" }.to_json
+        end
+        
+        File.read(file_path)
+      end
     end
   end
 end
