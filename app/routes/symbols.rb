05dc59b6ea5c9424bc1d923d444fd5b019bff724
Goran Mržljak <goran.mrzljak@gmail.com>
2026-02-15 20:24:05 +0100

    add symbols/meta route

--- app/routes/symbols.rb
+++ app/routes/symbols.rb
@@ -15,6 +15,18 @@ module Routes
           
           File.read(file_path)
         end
+
+        get "/meta" do
+          content_type :json
+          file_path = File.join(Config.eodhd_dir, "meta.json")
+          
+          unless File.exist?(file_path)
+            status 404
+            return { error: "File not found: #{file_path}" }.to_json
+          end
+          
+          File.read(file_path)
+        end
       end
     end
   end
