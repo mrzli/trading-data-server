54eae1c94636dc28bf333c17cc05b6abd2bf1aa0
Goran Mržljak <goran.mrzljak@gmail.com>
2026-03-05 21:10:35 +0100

    allow cors

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,17 @@
 
 require "sinatra/base"
 require "sinatra/namespace"
+require "rack/cors"
 
 class Application < Sinatra::Base
   register Sinatra::Namespace
+
+  use Rack::Cors do
+    allow do
+      origins "*"
+      resource "*", headers: :any, methods: %i[get post put patch delete options head]
+    end
+  end
 
   configure do
     set :show_exceptions, false if settings.environment == :production
